e4ec7e47bf0d6fc4bd246a7e48db7c39226fe2e2
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2023-02-04 00:34:56 +0530

    Committing project

---
+++ Therapy_App/app/src/main/java/com/example/therapyapp/Model/Datum.java
@@ -0,0 +1,15 @@
+package com.example.therapyapp.Model;
+
+import java.util.Date;
+
+public class Datum{
+    public int id;
+    public String name;
+    public String image_nm;
+    public String image_path;
+    public String profile;
+    public String status;
+    public Date created_at;
+    public Date updated_at;
+}
+
---
+++ Therapy_App/app/src/main/java/com/example/therapyapp/Model/Root.java
@@ -0,0 +1,11 @@
+package com.example.therapyapp.Model;
+
+import java.util.ArrayList;
+
+public class Root{
+    public int status;
+    public boolean success;
+    public String message;
+    public String token;
+    public ArrayList<Datum> data;
+}
